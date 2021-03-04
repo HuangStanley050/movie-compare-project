61b57a9973683d30a82e66a282e6f6d540ec9320
Stanley Huang <infamous_godhand@yahoo.com>
2021-03-05 09:24:47 +1100

    api hooked up

--- components/MovieLeft/index.js
+++ components/MovieLeft/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import Box from "@material-ui/core/Box";
 import BoxOffice from "../BoxOffice";
 import MovieInfo from "../MovieInfo";
@@ -6,6 +7,18 @@ import MovieInput from "../MovieInput";
 import Rating from "../Rating";
 
 const MovieLeft = () => {
+  useEffect(async () => {
+    const getMovie = async () => {
+      let result = await axios.get("http://www.omdbapi.com", {
+        params: {
+          apikey: process.env.NEXT_PUBLIC_MOVIE_API_KEY,
+          s: "avengers",
+        },
+      });
+      return result;
+    };
+    console.log(await getMovie());
+  }, []);
   return (
     <Box>
       <MovieInput />
