e1b2b715e9d074190546808393df2b7ec95c6b75
Stanley Huang <infamous_godhand@yahoo.com>
2021-03-04 11:50:02 +1100

    header done

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,15 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Header from "../components/Header";
 
 import styles from "./index.module.css";
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js Material UI Template
-        </Typography>
+    <Container maxWidth="lg">
+      <Box>
+        <Header />
       </Box>
     </Container>
   );
