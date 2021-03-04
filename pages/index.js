2b520f6dd62937b83198a21eb4d8d4ad40a0b1a8
Stanley Huang <infamous_godhand@yahoo.com>
2021-03-05 09:04:38 +1100

    layout done

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,38 @@ import Header from "../components/Header";
 import MovieLeft from "../components/MovieLeft";
 import MovieRight from "../components/MovieRight";
 import styles from "./index.module.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export default function Index() {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
       <Box>
         <Header />
       </Box>
-      <MovieLeft />
-      <MovieRight />
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <MovieLeft />
+          </Grid>
+          <Grid item xs={6}>
+            <MovieRight />
+          </Grid>
+        </Grid>
+      </div>
     </Container>
   );
 }
