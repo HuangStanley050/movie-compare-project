719e8ffe0cc087a6c57d44e67a3a3346f7a16eb6
Stanley Huang <infamous_godhand@yahoo.com>
2021-03-10 08:34:06 +1100

    refactor to catch errors

--- components/MovieInput.js
+++ components/MovieInput.js
@@ -4,32 +4,43 @@ import axios from "axios";
 import debounce from "debounce-promise";
 
 const MovieInput = () => {
-  const wait = 3000;
-  let timeOutId;
   const fetchData = async (inputValue) => {
-    let result = await axios.get("http://www.omdbapi.com", {
-      params: {
-        apikey: process.env.NEXT_PUBLIC_MOVIE_API_KEY,
-        s: inputValue,
-      },
-    });
-    return result;
+    let result;
+    try {
+      result = await axios.get("http://www.omdbapi.com", {
+        params: {
+          apikey: process.env.NEXT_PUBLIC_MOVIE_API_KEY,
+          s: inputValue,
+        },
+      });
+      return result;
+    } catch (err) {
+      return err;
+    }
   };
   const promiseOptions = (inputValue) => {
-    return new Promise((resolve) => {
+    const wait = 3000;
+    let timeOutId;
+
+    return new Promise((resolve, reject) => {
       let searchResult;
 
       if (timeOutId) {
         clearTimeout(timeOutId);
       }
+
       timeOutId = setTimeout(async () => {
-        searchResult = await fetchData(inputValue);
-        //console.log(searchResult);
-        const filter = searchResult.data.Search.map((title) => ({
-          value: title.Title,
-          label: title.Title,
-        }));
-        resolve(filter);
+        try {
+          searchResult = await fetchData(inputValue);
+
+          const filter = searchResult.data.Search.map((title) => ({
+            value: title.Title,
+            label: title.Title,
+          }));
+          resolve(filter);
+        } catch (err) {
+          reject(err);
+        }
       }, wait);
     });
   };
