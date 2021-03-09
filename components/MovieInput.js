95b2a2987aa1e4ad1baaf6f054fffff07bf062a3
Stanley Huang <infamous_godhand@yahoo.com>
2021-03-09 13:27:37 +1100

    delay search working

--- components/MovieInput.js
+++ components/MovieInput.js
@@ -4,8 +4,35 @@ import axios from "axios";
 import debounce from "debounce-promise";
 
 const MovieInput = () => {
-  const wait = 1000;
-
+  const wait = 3000;
+  let timeOutId;
+  const fetchData = async (inputValue) => {
+    let result = await axios.get("http://www.omdbapi.com", {
+      params: {
+        apikey: process.env.NEXT_PUBLIC_MOVIE_API_KEY,
+        s: inputValue,
+      },
+    });
+    return result;
+  };
+  const promiseOptions = (inputValue) => {
+    let searchResult;
+    if (timeOutId) {
+      clearTimeout(timeOutId);
+    }
+    timeOutId = setTimeout(async () => {
+      searchResult = await fetchData(inputValue);
+      console.log(searchResult);
+    }, wait);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([
+          { value: "green", label: "G" },
+          { value: "yellow", label: "Y" },
+        ]);
+      }, 1000);
+    });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -17,7 +44,7 @@ const MovieInput = () => {
           className="basic-single"
           classNamePrefix="select"
           name="search"
-          loadOptions={() => {}}
+          loadOptions={promiseOptions}
         />
       </form>
     </div>
